1ed2cf0fc7811597e4b1f4559aa58ec8ac9331ec
Elior Abaev <elior.abaev@gmail.com>
2025-01-10 20:28:33 +0200

    Update header + logo designs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,13 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header'; // Import the Header component
-import Balance from './components/Balance';
-import Income from './components/Income';
-import Expense from './components/Expense';
-import Suggestions from './components/Suggestions';
+import Header from './components/Header';
 import './static/css/index.css';
 
 function App() {
-    const [step, setStep] = useState(1);
-    const [balance, setBalance] = useState({ id: null, amount: '' });
-
-    const handleBalanceSubmit = (balance) => {
-        setBalance(balance);
-        setStep(2); // Move to the income step
-    };
-
-    const handleIncomeSubmit = () => {
-        setStep(3); // Move to the expense step
-    };
-
-    const handleExpenseSubmit = () => {
-        setStep(4); // Move to the suggestions step
-    };
-
     return (
         <Router>
             <div className="App">
-                <Header /> {/* Include the Header component */}
-                <main>
-                    {step === 1 && <Balance onSubmit={handleBalanceSubmit} />}
-                    {step === 2 && <Income onSubmit={handleIncomeSubmit} />}
-                    {step === 3 && <Expense onSubmit={handleExpenseSubmit} />}
-                    {step === 4 && <Suggestions balanceId={balance.id} />}
-                </main>
+                <Header />
             </div>
         </Router>
     );

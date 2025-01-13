19bfeb32e057df75c17660224ec53476821ea43e
Elior Abaev <elior.abaev@gmail.com>
2025-01-13 14:35:57 +0200

    Add aboutme page and update header link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../static/css/Header.css';
-import logo from '../static/images/logo_clean.png';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../static/css/Header.css";
+import logo from "../static/images/logo_clean.png";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <div className="header-container">
-        <div className="logo-container">
-          <a href="/" className="logo">
-            <img className="logo-image" src={logo} alt="Budget app logo" />
-          </a>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/about">About Me</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-};
+    const navigate = useNavigate(); // Initialize the navigate function
 
+    const handleAboutClick = () => {
+        navigate("/about"); // Navigate to the About Me page
+    };
+
+    const handleLogoClick = () => {
+        navigate("/"); // Navigate to the home page
+    };
+
+    return (
+        <header className="header">
+            <div className="header-container">
+                <div className="logo-container">
+                    <span className="logo" onClick={handleLogoClick}>
+                        <img className="logo-image" src={logo} alt="Budget app logo" />
+                    </span>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <span className="nav-link" onClick={handleAboutClick}>
+                                About Me
+                            </span>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
